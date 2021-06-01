88abcaa201f629c894e0098713e5154dbb5a8fc1
Koushik <mahantakoushik80@gmail.com>
2021-06-02 02:12:57 +0530

    ..

--- Let us C/Chapter 4/D@b.c
+++ Let us C/Chapter 4/D@b.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 int main()
 {
-char character, ASCIIc ;
+char character;int ASCIIc ;
 printf("What is the character?");
 scanf("%c",&character);
-ASCIIc = ("%d",character);
-if((65<=ASCIIc)&&(ASCIIc>=90))
+ASCIIc = character;
+if(ASCIIc<=90 && ASCIIc>=65)
     printf("The character is an upper case letter.");
-else if((97<=ASCIIc)&&(ASCIIc>=122))
+else if((ASCIIc>=97)&&(ASCIIc<=122))
     printf("The character is a lower case letter.");
-else if((48<=ASCIIc)&&(ASCIIc>=57))
+else if((ASCIIc<=57)&&(ASCIIc>=48))
     printf("The character is a digit.");
-else if(((0<=ASCIIc)&&(ASCIIc>=47))||((58<=ASCIIc)&&(ASCIIc>=64))||((91<=ASCIIc)&&(ASCIIc>=96))||((123<=ASCIIc)&&(ASCIIc>=123)))
+else if( ((ASCIIc>0)&&(ASCIIc<=47))   ||    ((58<=ASCIIc)&&(ASCIIc<=64))  ||     ((91<=ASCIIc)&&(ASCIIc=96))   ||  ((123<=ASCIIc)&&(ASCIIc<=123)))
     printf("The character is a special symbol.");
 return 0;
 }    
