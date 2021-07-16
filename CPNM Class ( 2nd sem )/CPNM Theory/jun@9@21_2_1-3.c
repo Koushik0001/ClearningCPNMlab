402362283d9e903fe3a561942480175ab8f895c4
Koushik Mahanta <77391839+Koushik0001@users.noreply.github.com>
2021-07-17 02:11:42 +0530

    Update jun@9@21_2_1-3.c

--- CPNM Class ( 2nd sem )/CPNM Theory/jun@9@21_2_1-3.c
+++ CPNM Class ( 2nd sem )/CPNM Theory/jun@9@21_2_1-3.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
  /* print Fahrenheit-Celsius table
- for fahr = 0, 20, ..., 300; floating-point version */
+ for fahr = 0, 20, ..., 280, 300; floating-point version*/
 int main()
 {
     float fahr, celsius;
